aead4d172433c9279a7acfcbdbb894e94943354a
Sp1kkelpoes <Lukaszvanhoogen@gmail.com>
2025-02-28 23:20:38 +0100

    Initial commit

---
+++ src/main/java/net/sp1kkelpoes/plentifulcreatures/PlentifulCreaturesClient.java
@@ -0,0 +1,10 @@
+package net.sp1kkelpoes.plentifulcreatures;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlentifulCreaturesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
